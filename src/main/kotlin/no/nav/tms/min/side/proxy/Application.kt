1aa131e52471f6dc37b76b939498ce5f31004f2f
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-12-15 15:39:32 +0100

    debug config

--- src/main/kotlin/no/nav/tms/min/side/proxy/Application.kt
+++ src/main/kotlin/no/nav/tms/min/side/proxy/Application.kt
@@ -1,18 +1,18 @@
 package no.nav.tms.min.side.proxy
 
-import io.ktor.client.HttpClient
-import io.ktor.server.engine.ApplicationEngineEnvironmentBuilder
 import io.ktor.server.engine.applicationEngineEnvironment
-import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import mu.KotlinLogging
 import no.nav.tms.min.side.proxy.config.Environment
 import no.nav.tms.min.side.proxy.config.HttpClientBuilder
 import no.nav.tms.min.side.proxy.config.mainModule
 
+private val log = KotlinLogging.logger {  }
 fun main() {
     val env = Environment()
     val httpClient = HttpClientBuilder.build()
+
 
     embeddedServer(factory = Netty, port =  8080) {
         applicationEngineEnvironment {
@@ -26,6 +26,8 @@ fun main() {
                 )
             }
         }
-    }.start(wait = true)
+    }.start(wait = true).also {
+        log.info { "Starter applikasjon med config ${it.environment.config.toMap()}" }
+    }
 }
 
