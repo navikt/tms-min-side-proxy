cf1ebd6a27542fba224e031171b3e6234f3e2421
Christopher Santana <christopher.santana@nav.no>
2023-10-06 14:18:19 +0200

    Henter navn direkte fra pdl.

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,4 +1,22 @@
+import default.DependencyGroup
 import default.KotlinDefaults
+
+object Caffeine: DependencyGroup {
+    override val groupId = "com.github.ben-manes.caffeine"
+    override val version = "3.1.8"
+
+    val caffeine = dependency("caffeine")
+}
+
+object GraphQL: DependencyGroup {
+    override val groupId get() = "com.expediagroup"
+    override val version = "6.3.5"
+    val pluginId get() = "com.expediagroup.graphql"
+
+    val kotlinClient get() = dependency("graphql-kotlin-client")
+
+    val kotlinKtorClient get() = dependency("graphql-kotlin-ktor-client")
+}
 
 object KotlinTest: KotlinDefaults {
     val junit = dependency("kotlin-test-junit")
