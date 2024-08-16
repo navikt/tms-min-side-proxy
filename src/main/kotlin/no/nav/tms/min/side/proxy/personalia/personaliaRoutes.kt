abcd853305d1147b46c6a6c98c01b8db5798409d
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-08-16 10:28:49 +0200

    Debug logging

--- src/main/kotlin/no/nav/tms/min/side/proxy/personalia/personaliaRoutes.kt
+++ src/main/kotlin/no/nav/tms/min/side/proxy/personalia/personaliaRoutes.kt
@@ -1,5 +1,6 @@
 package no.nav.tms.min.side.proxy.personalia
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -7,11 +8,15 @@ import io.ktor.util.pipeline.*
 import no.nav.tms.token.support.tokenx.validation.user.TokenXUserFactory
 
 fun Route.personaliaRoutes(personaliaFetcher: PersonaliaFetcher) {
+    val log = KotlinLogging.logger {}
+
     get("/personalia") {
         try {
+            log.info { "Inside personalia route" }
             personaliaFetcher.getNavn(user)
                 .let { navn -> call.respond(NavnAndIdent(navn, user.ident)) }
         } catch (e: HentNavnException) {
+            log.error { "Failed to fetch personalia" }
             call.respond(NavnAndIdent(navn = null, ident = user.ident))
         }
     }
